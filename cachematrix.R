708fbdec70c939ed4704b67f7b68fc092e724791
Gilser <klimkoalexandra@gmail.com>
2015-01-25 16:06:03 +0200

    message

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates cash for solve matrix operation
+## If this matrix have been cashed already, then function doesn't recalculate it
 
-## Write a short comment describing this function
+## Create a cash
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x) {
+  mx <- NULL
+  set <- function(y) {
+    x <<- y
+    mx <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) mx <<- solve
+  getsolve <- function() mx
+  list(set = set, get = get, getsolve = getsolve, setsolve = setsolve)
 }
 
 
-## Write a short comment describing this function
+## Implement a solve function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x = matrix(), ...) {
+  mx <- x$getsolve()
+  if(!is.null(mx)) {
+    message("getting cached data")
+    return(mx)
+  }
+  data <- x$get()
+  mx <- solve(data, ...)
+  x$setsolve(mx)
+  m
 }
+
